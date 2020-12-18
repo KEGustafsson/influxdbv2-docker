fa8733e0dea73aa3179865f33869b569fa0dec71
KE Gustafsson <ke.gustafsson@gmail.com>
2020-12-18 12:29:02 +0200

    test for influxdb

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # influxdbv2-docker
+
+Telegraf, Influxdb v2 and Grafana in docker for Signal K testing.
+
+If Signal K is not runnign is same computer, change telegraf.conf setting first. Also if you want to read other path than vessels.self.
+
+Run run_me_1st.sh and enter username, password, org and bucket for Influxdb.
+
+Influxdb port = 8087
+
+Grafana port = 3002
+
+When Influxdb is runnign, generate token from Grafana and add data source (Query Language = Flux).
---
+++ run_me_1st.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+mkdir -p $PWD/volume/influxdbv2
+mkdir -p $PWD/volume/grafana/data
+mkdir -p $PWD/volume/grafana/conf
+mkdir -p $PWD/volume/telegraf
+cp $PWD/telegraf.conf $PWD/volume/telegraf/telegraf.conf
+
+echo "Select Influxdb v2 username"
+read username
+echo
+echo "Select Influxdb v2 password"
+read password
+echo
+echo "Select Influxdb v2 organization"
+read org
+echo
+echo "Select Influxdb v2 bucket"
+read bucket
+echo
+
+docker-compose pull
+docker-compose up --no-start
+docker-compose start influxdbv2_test &
+
+sleep 10
+
+x=$(curl -X POST http://localhost:8087/api/v2/setup -H 'Content-Type: application/json' -d '{"username": "'$username'","password": "'$password'","org": "'$org'","bucket": "'$bucket'","retentionPeriodHrs": 0}')
+echo $x
+
+a=$(echo $x | jq '.auth.token')
+b=$(echo $x | jq '.org.name')
+c=$(echo $x | jq '.bucket.name')
+
+cat $PWD/volume/telegraf/telegraf.conf
+
+sed -i "s#\"\$INFLUX_TOKEN\"#$a#g" $PWD/volume/telegraf/telegraf.conf
+sed -i "s#\"\$ORG\"#$b#g" $PWD/volume/telegraf/telegraf.conf
+sed -i "s#\"\$BUCKET\"#$c#g" $PWD/volume/telegraf/telegraf.conf
+
+echo
+cat $PWD/volume/telegraf/telegraf.conf
+
+docker run --name grafana_files grafana/grafana:latest &
+sleep 30
+docker cp grafana_files:/var/lib/grafana/. $PWD/volume/grafana/data
+docker cp grafana_files:/usr/share/grafana/conf/. $PWD/volume/grafana/conf
+docker stop grafana_files
+docker rm grafana_files
+
+docker-compose down
+docker-compose up -d
